44246dbf5171437e3c17fd41e3b4ec76a0512b6e
rmbooth92084 <rmbooth92084@gmail.com>
2020-06-25 13:38:01 -0700

    made tests for bubble sort and redid bubble sort to use vectors

--- bubbleSort.h
+++ bubbleSort.h
@@ -1,29 +1,42 @@
+#ifndef __BUBBLESORT_HPP__
+#define __BUBBLESORT_HPP__
 /*
 	Author: Raymond Booth
 	Description: This is my implentation of bubble sort
 */
-int *BubbleSort(int list[])
+#include <iostream>
+#include <vector>
+std::vector<int> BubbleSort(std::vector<int> list)
 {
 	int num1, num2, counter;
+	
+	if(list.empty())//deals with an empty list
+		return list;
+	
 	while(1)
 	{
-		for(int i = 0; i < sizeof(list); i++)
+		for(int i = 0; i < list.size() - 1; i++)
 		{
-			if(list[i+1] < list[i])
+			if(list.at(i+1) < list.at(i))
 			{
-				num1 = list[i+1];
-				num2 = list[i];
+				num1 = list.at(i+1);
+				num2 = list.at(i);
 				
-				list[i] = num1;
-				list[i+1] = num2; //flips the the two numbers
+				list.at(i) = num1;
+				list.at(i+1) = num2; //flips the the two numbers
 			}
 			else
 				counter++;
 		}
-		if(counter == sizeof(list))// breaks out of while loop if array is sorted
+		if(counter == list.size() - 1)// breaks out of while loop if vector is sorted
 		{
+			/*for(int i = 0; i < list.size(); i++)//prints out the vector
+			{
+				std::cout << list.at(i) << std::endl;
+			}*/
 			return list;
 		}
 		counter = 0;
 	}
 }
+#endif
--- bubble_test.h
+++ bubble_test.h
@@ -2,17 +2,50 @@
 #define __BUBBLE_TEST_H__
 
 #include "bubbleSort.h"
+#include <vector>
 //This test makes sure that it can sort a basic list
 TEST(BubbleSort, SortingBasicList)
 {
-	int test[] = {9,8,7,6,5,4,3,2,1,0};
+	std::vector<int> test = {9,8,7,6,5,4,3,2,1,0};
 	int sorted[] = {0,1,2,3,4,5,6,7,8,9};
 	
-	for(int i = 0; i < sizeof(test); i++)
+	for(int i = 0; i < test.size() - 1;i++)
 	{
-		EXPECT_EQ(BubbleSort(test)[i],sorted[i]);
+		EXPECT_EQ(BubbleSort(test).at(i),sorted[i]);
 	}
 	
 }
 
+TEST(BubbleSort, AlreadySortedList)
+{
+	std::vector<int> test = {1,2,3,4,5};
+	int sorted[] = {1,2,3,4,5};
+	
+	for(int i = 0; i < test.size() - 1;i++)
+	{
+		EXPECT_EQ(BubbleSort(test).at(i),sorted[i]);
+	}
+	
+}
+
+TEST(BubbleSort, OneNumberLongList)
+{
+	std::vector<int> test = {0};
+	int sorted[] = {0};
+	
+	for(int i = 0; i < test.size() - 1;i++)
+	{
+		EXPECT_EQ(BubbleSort(test).at(i),sorted[i]);
+	}
+	
+}
+
+TEST(BubbleSort, EmptyList)
+{
+	std::vector<int> test = {};
+	
+	EXPECT_EQ(BubbleSort(test),test);
+
+	
+}
 #endif//__Bubble_test_h_
